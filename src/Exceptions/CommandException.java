a38e315327d97d20bedcde07f3e3b6989de28084
Ardian Bryan Limasarian <ardian-bryan@outlook.com>
2025-08-08 02:29:24 +0800

    version 1.5

--- src/Exceptions/CommandException.java
+++ src/Exceptions/CommandException.java
@@ -1,5 +1,8 @@
 package Exceptions;
 
+/**
+ * Handles Exceptions of type {@code CommandException} that may occur during command execution.
+ */
 public class CommandException extends RuntimeException {
     public CommandException(String message) {
         super(message);
